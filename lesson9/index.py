a1adda8914ea39ef0bac37667f380c388601dc27
楊浩東 <bondyhd@yahoo.com.tw>
2025-07-23 13:25:08 +0000

    modify

--- lesson9/index.py
+++ lesson9/index.py
@@ -1,39 +1,12 @@
-import random
+import tools  #import自行建立的module (必為py檔)
 
-def play_game(): #沒有呼叫就不會執行
-    min = 1
-    max = 100
-    count = 0
-    guess_value = random.randint(min,max)
-    print(guess_value)
-    print("==========猜數字遊戲===========\n")
-    while(True):
-            count += 1
-            keyin = int(input(f"猜數字範圍{min}~{max}:"))            
-            if keyin >= min and keyin <= max:
-                if(keyin == guess_value):
-                    print(f"賓果!猜對了,答案是:{guess_value}")
-                    print(f"您猜了{count}次")
-                    break
-                elif keyin > guess_value:
-                    print("再小一點!")
-                    max = keyin - 1
-                
-                else:
-                    print("再大一點")
-                    min = keyin + 1
-
-                print(f"您已經猜{count}次")
-                
-            else:
-                print("請輸入提示範圍內的數字")
 
 def main(): #try也可以放在主function裡
     try:
         play_count = 0  #在root區，一定會執行
         while(True):
             play_count += 1
-            play_game()  #呼叫play_game()這個function，如果有raise會直接從這個回圈跳出
+            tools.play_game()  #呼叫play_game()這個function，如果有raise會直接從這個回圈跳出
             is_continue = input("您還要繼續嗎(y,n)?")
             if is_continue == "n":
                 break
